74393c214c33d4a52f18b0d0365d58b66760f6cb
Cristian <128819507+fremencl@users.noreply.github.com>
2024-01-01 18:53:57 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,48 +1,49 @@
-import streamlit as st
+import json
+import time
+
 import requests
+import streamlit as st
 from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
 
-#Funcion para nuestra animacion
-def load_lottieurl(url):
-  r = requests.get(url)
-  if r.status_code != 200:
-    return None
-  return r.json()
+def load_lottiefile(filepath: str):
+    with open(filepath, "r") as f:
+        return json.load(f)
 
-# URL animacion
-lottie_coding = load_lottieurl("https://lottie.host/embed/3d1e0c61-ff21-4eb8-bf49-23aeae2609d0/sRiDPU44t3.json")
+# Rutas a los archivos Lottie y de imagen en tu carpeta 'lottiefiles'
+lottie_lentes = load_lottiefile("./lottiefiles/animalentes.json")
+url_imagen = "./lottiefiles/octica.png"
 
-# URL de la imagen
-url_imagen = "https://i.postimg.cc/Kcd2DR4s/Bit-Logo-Solo-2.png"
+st.set_page_config(
+    page_title="Optica Digital",
+    page_icon=":tada:",
+)
 
-with st.container():
-  st.subheader("Llegó el momento de probar tus lentes")
-  st.title("Tomemos una foto y veamos como te luce este modelo")
-  st.write("XXXXXXXX")
-
-with st.container():
-  st.write("---")
-  left_column, right_column = st.columns(2)
-  with left_column:
-    st.header("Mira la Cámara")
-    #st.write(
-      #"""
-        #Texto aquí
-      #"""
-    #)
-  with right_column:
-    st_lottie(lottie_coding, height=300, key="coding")
+st.title("Llegó el momento de probar tus lentes!")
+st.markdown(
+    """
+Ahora puedes ver cómo te quedarán este modelo de lentes, si no te gustan no es problema, puedes elegir otro modelo y probártelo digitalmente. La idea es que encuentres el modelo que mejor luce en ti.
+"""
+)
 
 with st.container():
-  st.write("--")
-  st.header("Te gustó como te ves con estos lentes?")
-  image_column, text_column = st.columns((1, 2))
-  with image_column:
-    st.image(url_imagen, use_column_width=True)
-  with text_column:
-    st.write(
-      """
-      ECONOPTICA - Somos tu optica en linea con los mejores precios
-      """
-    )
-    st.markdown("[Nuestra Tienda web...](https:///juguetesingenium.cl)")
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("Mira la Cámara")
+    with right_column:
+        st_lottie(lottie_lentes, height=300, key="lentes")
+
+with st.container():
+    st.write("--")
+    st.header("Te gustó como te ves con estos lentes?")
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(url_imagen, use_column_width=True)
+    with text_column:
+        st.write(
+            """
+            ECONOPTICA - Somos tu óptica en línea con los mejores precios
+            """
+        )
+        st.markdown("[Nuestra Tienda web...](https:///juguetesingenium.cl)")
