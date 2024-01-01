37a8301fcca18a29f7fc7729a79c12b18a19cdd7
Cristian <128819507+fremencl@users.noreply.github.com>
2024-01-01 19:45:53 -0300

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ import time
 
 import requests
 import streamlit as st
-#from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie
 #from streamlit_lottie import st_lottie_spinner
 
 def load_lottiefile(filepath: str):
