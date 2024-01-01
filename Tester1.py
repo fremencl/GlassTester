af5f917737ac9ea53c8db31ffdf9104a8818387d
Cristian <128819507+fremencl@users.noreply.github.com>
2024-01-01 16:58:22 -0300

    Update Tester1.py

--- Tester1.py
+++ Tester1.py
@@ -1,46 +1,34 @@
 import streamlit as st
 import requests
 from streamlit_lottie import st_lottie
-from PIL import Image
 
-#Funcion para nuestra animacion
+# Función para cargar la animación Lottie desde una URL
 def load_lottieurl(url):
-  r = requests.get(url)
-  if r.status_code != 200:
-    return None
-  return r.json()
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-lottie_coding = load_lottieurl("https://lottie.host/embed/3d1e0c61-ff21-4eb8-bf49-23aeae2609d0/sRiDPU44t3.json")
-imagen_video = Image.open("/content/Octica.png")
+# URL de la imagen
+url_imagen = "https://i.postimg.cc/Kcd2DR4s/Bit-Logo-Solo-2.png"
 
-with st.container():
-  st.subheader("Llegó el momento de probar tus lentes")
-  st.title("Tomemos una foto y veamos como te luce este modelo")
-  st.write("XXXXXXXX")
+# URL de la animación Lottie
+url_lottie = "https://lottie.host/embed/3d1e0c61-ff21-4eb8-bf49-23aeae2609d0/sRiDPU44t3.json"
 
-with st.container():
-  st.write("---")
-  left_column, right_column = st.columns(2)
-  with left_column:
-    st.header("Mira la Cámara")
-    #st.write(
-      #"""
-        #Texto aquí
-      #"""
-    #)
-  with right_column:
-    st_lottie(lottie_coding, height=300, key="coding")
+# Encabezado y título
+st.subheader("Llegó el momento de probar tus lentes")
+st.title("Tomemos una foto y veamos cómo te luce este modelo")
 
-with st.container():
-  st.write("--")
-  st.header("Te gustó como te ves con estos lentes?")
-    image_column, text_column = st.columns((1, 2))
-  with image_column:
-    st.image(imagen_video)
-  with text_column:
-    st.write(
-      """
-      ECONOPTICA - Somos tu optica en linea con los mejores precios
-      """
-    )
-    st.markdown("[Nuestra Tienda web...](https:///juguetesingenium.cl)")
+# Lottie
+lottie_coding = load_lottieurl(url_lottie)
+st_lottie(lottie_coding, height=300, key="coding")
+
+# Te gustó como te ves con estos lentes?
+st.header("¿Te gustó cómo te ves con estos lentes?")
+
+# Mostrar la imagen desde la URL
+st.image(url_imagen, use_column_width=True)
+
+# Texto adicional
+st.write("ECONOPTICA - Somos tu óptica en línea con los mejores precios")
+st.markdown("[Nuestra Tienda web...](https://juguetesingenium.cl)")
